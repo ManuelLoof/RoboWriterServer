0a4362a03dd61c146ed12671fc704272764efc40
Manuel Loof <manuel.loof@gmx.de>
2018-02-28 23:29:27 +0100

    Scripts open in differnt colors.

--- RoboWriter/Tooling/startConsoles.ps1
+++ RoboWriter/Tooling/startConsoles.ps1
@@ -1,10 +1,10 @@
 # starts the powershell consoles for git, dotnet and neo4j
 
 # -> Git
-Start-Process powershell ..  
+Start-Process powershell -ArgumentList  "-NoExit cd.. | blue.ps1"
 
 # -> dotnet
-Start-Process powershell ..
+Start-Process powershell  "-NoExit cd.. | magenta.ps1"
 
 #-> neo4j
-Start-Process powershell c:\tools\neo4j-community\neo4j-community-3.2.6\bin\
+Start-Process powershell "-NoExit cd c:\tools\neo4j-community\neo4j-community-3.2.6\bin\"
